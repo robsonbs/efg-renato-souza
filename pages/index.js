3ee4b6e524731e229a63ba327c6035f82c5ce136
Robson Barbosa Souza <powerrob92@gmail.com>
2024-03-29 00:31:18 +0000

    chore: install next

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <h1>Aulas de UX/UI</h1>
+      <section>
+        <h2>Introdução</h2>
+        <a href="./ux-ui-aula001.pdf">Introdução</a>
+      </section>
+    </>
+  );
+}
+export default Home;
