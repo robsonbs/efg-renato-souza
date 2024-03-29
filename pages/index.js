f1fdce49da7467c1c6a4589b6da01cbbd953319d
Robson Barbosa Souza <powerrob92@gmail.com>
2024-03-29 00:45:27 +0000

    chore: update a link

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,13 @@ function Home() {
       <h1>Aulas de UX/UI</h1>
       <section>
         <h2>Introdução</h2>
-        <a href="files/ux-ui-aula001.pdf">Introdução</a>
+        <a
+          href="files/ux-ui-aula001.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Introdução
+        </a>
       </section>
     </>
   );
