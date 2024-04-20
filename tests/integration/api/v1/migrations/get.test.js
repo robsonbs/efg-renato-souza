e4f045a67d185d57f8b09b6e0cddcbbd1b987506
Robson Barbosa Souza <powerrob92@gmail.com>
2024-04-20 17:31:52 +0000

    chore: add test of api routes

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,14 @@
+import { cleanDatabase } from "./index.js";
+
+beforeAll(cleanDatabase);
+
+test("GET /api/v1/migrations  should return status 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+
+  expect(response.status).toEqual(200);
+
+  const responseBody = await response.json();
+
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
---
+++ tests/integration/api/v1/migrations/others.test.js
@@ -0,0 +1,28 @@
+import { cleanDatabase } from "./index.js";
+
+async function getMigrationsResponse(method) {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method,
+  });
+  return response;
+}
+
+async function getDataBaseStatus() {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  return await response.json();
+}
+
+beforeAll(cleanDatabase);
+
+test("OTHER HTTP METHODS to /api/v1/migrations should not let opened connections in database", async () => {
+  for (let method of ["HEAD", "PUT", "DELETE", "OPTIONS", "PATCH"]) {
+    await cleanDatabase();
+
+    const migrationsResponse = await getMigrationsResponse(method);
+    // console.log(`{method: ${method}, status:${migrationsResponse.status}}`);
+    expect(migrationsResponse.status).toBe(405);
+
+    const status = await getDataBaseStatus();
+    expect(status.dependencies.database.opened_connections).toEqual(1);
+  }
+});
