5bafcb2c07430141774f98cb5cf0e6a1b19868d3
Robson Barbosa Souza <powerrob92@gmail.com>
2024-04-20 21:38:06 +0000

    chore: add contents indexes

---
+++ infra/migrations/1713641148819_alter-contents-table-add-root-id-array.js
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+
+exports.up = async (pgm) => {
+  await pgm.addColumns("contents", {
+    path: {
+      type: "uuid[]",
+      notNull: true,
+      default: "{}",
+    },
+  });
+
+  await pgm.createIndex("contents", "path", {
+    name: "contents_path_idx",
+    method: "gin",
+  });
+};
+
+exports.down = false;
