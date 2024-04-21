4a716fc5717be05816ea4546249adcab754ca8b8
Robson Barbosa Souza <powerrob92@gmail.com>
2024-04-21 13:44:59 +0000

    refact: improve calls to database on `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,21 +3,19 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const versionResult = await database.query("SHOW server_version;");
-  const versionResultValue = versionResult.rows[0].server_version;
-
-  const [maxConnectionsResult, superuserReservedConnectionsResult] =
-    await database.query(
-      "SHOW max_connections; SHOW superuser_reserved_connections;"
-    );
-  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
-  const superuserReservedConnectionsValue =
-    superuserReservedConnectionsResult.rows[0].superuser_reserved_connections;
-  const openConnectionsResult = await database.query({
-    text: "SELECT numbackends as opened_connections FROM pg_stat_database where datname = $1",
+  const queryResult = await database.query({
+    text: `SELECT 
+    current_setting('server_version')  AS server_version,
+    cast(current_setting('max_connections')AS INT) AS max_connections,
+    cast(current_setting('superuser_reserved_connections') AS INT) AS reserved_connections,
+    cast((SELECT count(pid) FROM pg_stat_activity WHERE state = 'active' and datname = $1)AS INT) AS opened_connections;`,
     values: [process.env.POSTGRES_DB],
   });
-  const openConnectionsValue = openConnectionsResult.rows[0].opened_connections;
+  const versionResultValue = queryResult.rows[0].server_version;
+  const maxConnectionsValue = queryResult.rows[0].max_connections;
+  const superuserReservedConnectionsValue =
+    queryResult.rows[0].reserved_connections;
+  const openConnectionsValue = queryResult.rows[0].opened_connections;
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
@@ -25,8 +23,7 @@ async function status(request, response) {
         status: "healthy",
         version: versionResultValue,
         max_connections:
-          parseInt(maxConnectionsValue) -
-          parseInt(superuserReservedConnectionsValue),
+          maxConnectionsValue - superuserReservedConnectionsValue,
         opened_connections: openConnectionsValue,
       },
     },
