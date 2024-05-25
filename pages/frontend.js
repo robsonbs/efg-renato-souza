ab0a41a2c522e4b8bb4f0e684add1fe6bfbffaee
Robson Souza <robson.souza@olgari.com.br>
2024-05-24 21:09:45 -0300

    chore: update project

--- pages/frontend.js
+++ pages/frontend.js
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import styles from "./pages.module.css";
-import Home from "pages";
 function Frontend() {
   return (
     <>
-      <h1>Aulas de Front</h1>
+      <h1>Aulas de Frontend</h1>
       <section className={styles.section}>
         <article>
           <h2>Introdução as CSS</h2>
@@ -19,7 +18,19 @@ function Frontend() {
             Slides
           </a>
         </article>
-        
+        <article>
+          <h2>Página de receita</h2>
+          <span>
+            Vamos combinar os conhecimentos, hora de misturar Frontend e UI.
+          </span>
+          Modelo: <a
+            href="https://www.figma.com/community/file/1360315130061454535/pagina-de-receita"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Design Figma
+          </a> Desafio: <a href="https://www.figma.com/design/TXbGJm53NFoAjA0cadFL0v/Receita-Junina-%E2%80%A2-Desafio-26-(Community)" target="_blank" rel="noopener noreferrer">Referência Figma</a>
+        </article>
         <Link href="/"> Aulas de UX-UI</Link>
       </section>
     </>
