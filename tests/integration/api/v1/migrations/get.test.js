a8e1dd4527c4dc344dbada9ac500486d4f484132
Robson Souza <powerrob92@gmail.com>
2024-07-04 22:26:57 -0300

    build: adds `Automated Tests` Github Action
    test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -2,12 +2,11 @@ import orchestrator from "tests/orchestrator.js";
 import { cleanDatabase } from "./index.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  cleanDatabase();
+  await cleanDatabase();
 });
 
 test("GET /api/v1/migrations  should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
-
   expect(response.status).toEqual(200);
 
   const responseBody = await response.json();
